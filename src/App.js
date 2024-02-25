eb6e5b255024556259e002b3919d84c4d797b1d5
shellypao <pao.r@northeastern.edu>
2024-02-25 07:35:53 -0500

    notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Form from './components/Form';
 import Main from './components/Main';
@@ -7,19 +7,19 @@ import Error from './components/Error';
 import Review from './components/Review';
 
 
-function App() {
+export default function App() {
   return (
     <div>
-    <BrowserRouter>
-        <Router>
-        <Route path="/" exact component={Main} />
-        <Route path="/form" component = {Form}/>
-        <Route path="/error" component={Error}/>
-        <Route path="/reviews" component={Review}/>
-        </Router>
-    </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" exact component={Main} />
+          <Route path="/main" exact component={Review} />
+          <Route path="/main" exact component={Error} />
+          {/* <Route path="/form" component = {Form}/> */}
+          <Route path="/error" component={Error}/>
+          <Route path="/reviews" component={Form}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-export default App;
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 
 
 // Component for the main page
-export default function Main() {
-  return (
-    <div>
-      <h1>Welcome!</h1>
-      <p>This is the main page.</p>
-      <Link to="/Form">
-        <button>Write a Review!</button>
-      </Link>
-    </div>
-  );
-}
+//export default function Main() {
+//   return (
+//     <div>
+//       <h1>Welcome!</h1>
+//       <p>This is the main page.</p>
+//         <button>Write a Review!</button>
+//     </div>
+//   );
+//}
+
+export default function LoginLayout() {
+  
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+      let path = `/Form`; 
+      navigate(path);
+    }
+    
+    return (
+       <div className="app flex-row align-items-center">
+        <container> 
+            <button color="primary" className="px-4"
+              onClick={routeChange}
+                >
+                Write a review!
+              </button>
+         </container>
+      </div>
+    );
+  }
 
